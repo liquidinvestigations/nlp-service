57fe3feee664d66676ed270b6953788d0c7bbcfe
MattesR <mattes.ruckdeschel@tuhh.de>
2021-02-05 16:13:49 +0100

    flake8 changes

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-from numpy.core.function_base import add_newdoc
 import spacy
 import re
 from polyglot.text import Text
@@ -8,7 +7,7 @@ from flask import Flask, request, jsonify, abort
 nlp = spacy.load('xx_ent_wiki_sm')
 nlp_en = spacy.load('en_core_web_sm')
 supported_ner_polyglot = [name for name in os.listdir("/root/polyglot_data/ner2")]
-supported_ner_spacy = ['nl','en','fr','de','it','pl','pt','ru','es']
+supported_ner_spacy = ['nl', 'en', 'fr', 'de', 'it', 'pl', 'pt', 'ru', 'es']
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
@@ -22,9 +21,10 @@ def get_language(text):
     else:
         return None
 
+
 @app.route('/entity_extraction', methods=['POST'])
 def get_entities():
-    if not request.json or not 'text' in request.json:
+    if not request.json or 'text' not in request.json:
         abort(400)
     text = request.json['text']
     method = 'spacy'
@@ -37,37 +37,46 @@ def get_entities():
         lan = get_language(text[:2500])
         if lan == 'en':
             doc = nlp_en(text)
-            ents = [{'text': ent.text, 'start': ent.start_char, 'end': ent.end_char, 'label': ent.label_} for ent in doc.ents]
+            ents = [{'text': ent.text, 'start': ent.start_char,
+                    'end': ent.end_char, 'label': ent.label_}
+                    for ent in doc.ents]
         elif lan in supported_ner_spacy:
             doc = nlp(text)
-            ents = [{'text': ent.text, 'start': ent.start_char, 'end': ent.end_char, 'label': ent.label_} for ent in doc.ents]
+            ents = [{'text': ent.text, 'start': ent.start_char,
+                    'end': ent.end_char, 'label': ent.label_}
+                    for ent in doc.ents]
         elif lan in supported_ner_polyglot:
             method = 'polyglot'
             poly_text = Text(text, hint_language_code=lan)
             ents = []
             spans = []
-            # Each entity is only once inside the list and then matched. This is done because Polyglot only knows words, not char indexes.
-            entity_text = list(set([(' '.join(entity),entity.tag.lstrip('I-')) for entity in poly_text.entities]))
+            # Each entity is only once inside the list and then matched.
+            # This is done because Polyglot only knows words, not char indexes.
+            entity_text = list(set([(' '.join(entity), entity.tag.lstrip('I-'))
+                                    for entity in poly_text.entities]))
             entity_text.sort(key=len, reverse=True)
             for entity in entity_text:
                 for match in re.finditer(entity[0], text):
-                    # the match is only a match if it's not inside another match. Therefore, the entities are ordered by length beforehand.
-                    if all(not(span[0] <= match.start() <= span[1]) and not(span[0] <= match.end() <= span[1]) for span in spans):
-                        spans.append((match.start(),match.end()))
-                        ents.append({'text': entity[0], 'start': match.start(), 'end': match.end(), 'label': entity[1]})
+                    # the match is only a match if it's not inside another match.
+                    # Therefore, the entities are ordered by length beforehand.
+                    if all(not(span[0] <= match.start() <= span[1])
+                            and not(span[0] <= match.end() <= span[1]) for span in spans):
+                        spans.append((match.start(), match.end()))
+                        ents.append({'text': entity[0], 'start': match.start(),
+                                    'end': match.end(), 'label': entity[1]})
         else:
             method = 'not supported'
             ents = None
-        response = {'language': lan, 'method':method, 'entities': ents}
+        response = {'language': lan, 'method': method, 'entities': ents}
         return jsonify(response), 200
-                    
+
+
 @app.route('/language_detection', methods=['POST'])
 def return_language():
-    if not request.json or not 'text' in request.json:
+    if not request.json or 'text' not in request.json:
         abort(400)
-    language =  get_language(request.json['text'])
+    language = get_language(request.json['text'])
     return jsonify({'language': language}), 200
-
 
 
 if __name__ == "__main__":
--- test_api.py
+++ test_api.py
@@ -3,6 +3,7 @@ import requests
 
 NLP_URL = 'http://127.0.0.1:5001/'
 
+
 def call_nlp_server(endpoint, data_dict):
     url = urljoin(NLP_URL, endpoint)
     resp = requests.post(url, json=data_dict)
@@ -11,10 +12,11 @@ def call_nlp_server(endpoint, data_dict):
         raise RuntimeError(f'Unexpected response from nlp service: {resp.content}')
     return resp
 
+
 def get_entities_from_service(text, language=None):
     data = {'text': text}
     if language:
-        data['language']= language
+        data['language'] = language
     resp = call_nlp_server('entity_extraction', data)
     entity_list = resp.json()
     return entity_list
