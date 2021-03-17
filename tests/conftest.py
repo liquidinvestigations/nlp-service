76301951cc402b63fe2406de255e26e87dc8679d
MattesR <mattes.ruckdeschel@tuhh.de>
2021-03-17 16:01:19 +0100

    remove test with container, download only necessary embeddings

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,15 +8,3 @@ def app():
     app = get_app()
     return app
 
-
-def pytest_addoption(parser):
-    parser.addoption(
-        "--hostname",
-        action="store",
-        default='http://127.0.0.1:5000/'
-    )
-
-
-@pytest.fixture()
-def hostname(request):
-    return request.config.getoption("--hostname")
